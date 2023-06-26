d31b9dc000eae365c7dcad49042bf77150facac1
victorojedeji <ojedejivictordaniel@gmail.com>
2023-06-26 15:42:18 +0100

    announcement added

--- src/Components/Announcement.jsx
+++ src/Components/Announcement.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Announcement = () => {
   return (
-    <div className='flex justify-center content-center p-1.5 bg-gray-100'>
-      <p className='text-center'>Free shipping for orders in the US over $150</p>
+    <div className='flex justify-center content-center p-3 bg-gray-100'>
+      <p className='text-center text-sm font-light'>Free shipping for orders in the US over $150</p>
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+// import {} from 'react-icons'
 
 const Navbar = () => {
   return (
-    <div className='w-full p-2'>
-      Navbar
+    <div className='w-full px-6 py-8 '>
+      
     </div>
   )
 }
