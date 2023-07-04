038f17e7b9234d1a2be671d037fbd975a44c7da1
victorojedeji <ojedejivictordaniel@gmail.com>
2023-07-04 17:24:31 +0100

    giftshopsection added

---
+++ src/Components/GiftShopSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const GiftShopSection = () => {
+  return (
+    <div>
+      <div className='img-holder'>
+            
+      </div>
+  
+      <div>
+        <div className='w-full after:absolute after:block after:w-7 after:bg-black after:h-1 mb-5'>
+          <p className='text-2xl font-title py-1 text-3xl'>Gifts + Accessories</p>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default GiftShopSection
