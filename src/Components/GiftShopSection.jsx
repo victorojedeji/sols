a612d2a180c69f9f61397afa6b073d19c814e2dc
victorojedeji <ojedejivictordaniel@gmail.com>
2023-07-06 05:48:07 +0100

    styling giftShopSection

--- src/Components/GiftShopSection.jsx
+++ src/Components/GiftShopSection.jsx
@@ -6,13 +6,13 @@ const GiftShopSection = () => {
       <div className='giftshop-img-holder w-[60%] h-full'></div>
   
       <div className='w-[40%] relative'>
-        <div className='absolute bg-white pt-8 pl-6 pr-6 top-1/2 transform -translate-y-1/2 left-[-5rem]'>
+        <div className='absolute bg-white pt-8 pb-16 pl-6 pr-6 top-1/2 transform -translate-y-1/2 left-[-5rem]'>
           <div className='w-full after:absolute after:block after:w-7 after:bg-black after:h-1 mb-5'>
             <h5 className='text-2xl font-title py-1 text-3xl'>Gifts + Accessories</h5>
           </div>
           <p className='pt-3 pb-3'>Shop our collection of alpaca throws, jewelry, candles, and more.</p>
-          <div className='w-full relative'>
-            <button className='shop-gift-btn absolute bg-black text-white py-3 px-4'>shop gifts</button>
+          <div className='w-full relative h-auto'>
+            <button className='shop-gift-btn absolute bg-black text-white py-4 px-10 right-[-2em] capitalize'>shop gifts</button>
           </div>
         </div>
     </div>
