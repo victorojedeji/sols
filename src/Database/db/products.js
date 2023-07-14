0c5269386ba4f1f0dc7b17c5969bd47ea289dd2d
victorojedeji <ojedejivictordaniel@gmail.com>
2023-07-14 16:50:54 +0100

    products added

--- src/Database/db/products.js
+++ src/Database/db/products.js
@@ -164,39 +164,73 @@ import tote_2_2 from "../../Assets/products/tote bags/tote_2_2.jpg";
 function getRandomNumber() {
     const randomNumber = Math.round((Math.random() * (5 - 3.8) + 3.8) * 10) / 10;
     return randomNumber;
-  }
+}
+
+export const landingPage = [
+    {
+        name: "Citlalmina",
+        images: [flat_6_r, flat_6_l],
+        brand: "chris",
+        quantity: 3,
+        price: "150",
+        category: "flats",
+        reviewRate: getRandomNumber(),
+        description: "Lorem consectetur adipisicing elit. At voluptatibus incidunt quam cupiditate, eligendi perferendis, voluptate velit.",
+        fit: "Check out our fit guide for your size recommendation!",
+        sizesAvailable: [35, 36, 37, 38, 39, 40, 41, 42],
+        details: {
+            heelHeight: "0.75\"",
+            materials: {
+                upper: "leather",
+                insole: "leather",
+                outsole: "rubber",
+            },
+        },
+    },
+    {
+        name: "Cihuapilli",
+        images: [flat_7_r, flat_7_l],
+        brand: "chris",
+        quantity: 5,
+        price: "110",
+        category: "flats",
+        reviewRate: getRandomNumber(),
+        description: "Lorem consectetur adipisicing elit. At voluptatibus incidunt quam cupiditate, eligendi perferendis, voluptate velit.",
+        fit: "Check out our fit guide for your size recommendation!",
+        sizesAvailable: [35, 36, 37, 38, 39, 40, 41, 42],
+        details: {
+            heelHeight: "0.75\"",
+            materials: {
+                upper: "leather",
+                insole: "leather",
+                outsole: "rubber",
+                heel: "rubber",
+            },
+        },
+    },
+    {
+        name: "Jimena",
+        images: [flat_2_r, flat_2_l],
+        brand: "chris",
+        quantity: 2,
+        price: "100",
+        category: "flats",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At voluptatibus incidunt quam cupiditate, eligendi perferendis, voluptate velit.",
+        fit: "Check out our fit guide for your size recommendation!",
+        sizesAvailable: [35, 36, 37, 38, 39, 40, 41, 42],
+        details: {
+            heelHeight: "0.5\"",
+            materials: {
+                upper:"leather",
+                insole: "leather",
+                outsole: "rubber",
+            },
+        },
+    },
+]
 
 export const shoeProducts = [
-    {
-        name: "teagan alpaca throw",
-        images: [alpacathrow_1_1, alpacathrow_1_2],
-        brand: "ryzen",
-        quantity: 4,
-        price: "70",
-        category: "alpaca throws",
-        reviewRate: 0,
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quisquam laboriosam velit fugit, minima autem!",
-        fit: "Check out our fit guide for your size recommendation!",
-        details: {
-            throwSize: '110 cm x 165 cm',
-            materials: "65% alpaca, 30% wool, 5% nylon",
-        }
-    },
-    {
-        name: "christean soft alpaca throw",
-        images: [alpacathrow_2_1, alpacathrow_2_2],
-        brand: "ryzen",
-        quantity: 4.5,
-        price: "135",
-        category: "alpaca throws",
-        reviewRate: 4.5,
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
-        fit: "Check out our fit guide for your size recommendation!",
-        details: {
-            throwSize: '110 cm x 165 cm',
-            materials: "65% alpaca, 30% wool, 5% nylon",
-        }
-    },
     {
         name: "elle",
         images: [boot_1_r, boot_1_l],
@@ -847,5 +881,313 @@ export const shoeProducts = [
                 outsole: "20% recycled rubber, 80% natural rubber",
             },
         },
+    },
+]
+
+export const accessories = [
+    {
+        name: "teagan alpaca throw",
+        images: [alpacathrow_1_1, alpacathrow_1_2],
+        brand: "ryzen",
+        quantity: 4,
+        price: "70",
+        category: "alpaca throws",
+        reviewRate: 0,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quisquam laboriosam velit fugit, minima autem!",
+        fit: "Check out our fit guide for your size recommendation!",
+        details: {
+            throwSize: '110 cm x 165 cm',
+            materials: "65% alpaca, 30% wool, 5% nylon",
+        }
+    },
+    {
+        name: "christean soft alpaca throw",
+        images: [alpacathrow_2_1, alpacathrow_2_2],
+        brand: "ryzen",
+        quantity: 4.5,
+        price: "135",
+        category: "alpaca throws",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        fit: "Check out our fit guide for your size recommendation!",
+        details: {
+            throwSize: '110 cm x 165 cm',
+            materials: "65% alpaca, 30% wool, 5% nylon",
+        }
+    },
+    {
+        name: "chain choker",
+        images: [jewelry_1_1, jewelry_1_2],
+        brand: "ryzen",
+        quantity: 4,
+        price: "60",
+        category: "jewelry",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        fit: "Check out our fit guide for your size recommendation!",
+        details: {
+            materials: "18k gold vermeil",
+        }
+    },
+    {
+        name: "hollow hoop ear ring",
+        images: [jewelry_2],
+        brand: "ryzen",
+        quantity: 3,
+        price: "160",
+        category: "jewelry",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        fit: "Check out our fit guide for your size recommendation!",
+        details: {
+            materials: "18k gold vermeil",
+        }
+    },
+    {
+        name: "big arched studs",
+        images: [jewelry_3_1, jewelry_3_2],
+        brand: "ryzen",
+        quantity: 3,
+        price: "100",
+        category: "jewelry",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        fit: "Check out our fit guide for your size recommendation!",
+        details: {
+            materials: "18k gold vermeil",
+        }
+    },
+    {
+        name: "big arched studs",
+        images: [jewelry_3_1, jewelry_3_2],
+        brand: "ryzen",
+        quantity: 4,
+        price: "100",
+        category: "jewelry",
+        reviewRate:  getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        fit: "Check out our fit guide for your size recommendation!",
+        details: {
+            materials: "18k gold vermeil",
+        }
+    },
+    {
+        name: "big arched studs",
+        images: [jewelry_4_1, jewelry_4_2],
+        brand: "ryzen",
+        quantity: 5,
+        price: "100",
+        category: "jewelry",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        fit: "Check out our fit guide for your size recommendation!",
+        details: {
+            materials: "brass",
+        }
+    },
+    {
+        name: "bar studs",
+        images: [jewelry_5_1, jewelry_5_2],
+        brand: "ryzen",
+        quantity: 5,
+        price: "90",
+        category: "jewelry",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        fit: "Check out our fit guide for your size recommendation!",
+        details: {
+            materials: "18k gold vermeil",
+        }
+    },
+    {
+        name: "beaded chain choker",
+        images: [jewelry_6_1, jewelry_6_2],
+        brand: "ryzen",
+        quantity: 3,
+        price: "50",
+        category: "jewelry",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        fit: "Check out our fit guide for your size recommendation!",
+        details: {
+            materials: "14k gold filled, spring ring clasp",
+        }
+    },
+    {
+        name: "Non-Toxic Nail Polish",
+        images: [nail_1_1, nail_1_2],
+        brand: "racheal Bentinit",
+        quantity: 3,
+        price: "19",
+        category: "nail polish",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        ingredients: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam expedita debitis illum totam esse, libero illo laborum. Doloremque ducimus quae vel labore unde, aliquid excepturi esse fugiat, nostrum fugit libero iusto? Dolorum, aliquid ipsum. Illum officiis delectus nulla repellendus corporis."
+    },
+    {
+        name: "Non-Toxic Nail Polish",
+        images: [nail_2_1, nail_2_2],
+        brand: "racheal Bentinit",
+        quantity: 3,
+        price: "19",
+        category: "nail polish",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        ingredients: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam expedita debitis illum totam esse, libero illo laborum. Doloremque ducimus quae vel labore unde, aliquid excepturi esse fugiat, nostrum fugit libero iusto? Dolorum, aliquid ipsum. Illum officiis delectus nulla repellendus corporis."
+    },
+    {
+        name: "Non-Toxic Nail Polish",
+        images: [nail_3_1, nail_3_2],
+        brand: "racheal Bentinit",
+        quantity: 3,
+        price: "19",
+        category: "nail polish",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        ingredients: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam expedita debitis illum totam esse, libero illo laborum. Doloremque ducimus quae vel labore unde, aliquid excepturi esse fugiat, nostrum fugit libero iusto? Dolorum, aliquid ipsum. Illum officiis delectus nulla repellendus corporis."
+    },
+    {
+        name: "Non-Toxic Nail Polish",
+        images: [nail_4_1, nail_4_2],
+        brand: "racheal Bentinit",
+        quantity: 3,
+        price: "19",
+        category: "nail polish",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        ingredients: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam expedita debitis illum totam esse, libero illo laborum. Doloremque ducimus quae vel labore unde, aliquid excepturi esse fugiat, nostrum fugit libero iusto? Dolorum, aliquid ipsum. Illum officiis delectus nulla repellendus corporis."
+    },
+    {
+        name: "Non-Toxic Nail Polish",
+        images: [nail_5_1, nail_5_2],
+        brand: "racheal Bentinit",
+        quantity: 3,
+        price: "19",
+        category: "nail polish",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        ingredients: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam expedita debitis illum totam esse, libero illo laborum. Doloremque ducimus quae vel labore unde, aliquid excepturi esse fugiat, nostrum fugit libero iusto? Dolorum, aliquid ipsum. Illum officiis delectus nulla repellendus corporis."
+    },
+    {
+        name: "Augaustine pillow",
+        images: [pillow_1_1, pillow_1_2],
+        brand: "Bentinit",
+        quantity: 3,
+        price: "80",
+        category: "pillow",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam expedita debitis illum totam esse, libero illo laborum. Doloremque ducimus quae vel labore unde, aliquid excepturi esse fugiat, nostrum fugit libero iusto? Dolorum, aliquid ipsum. Illum officiis delectus nulla repellendus corporis."
+    },
+    {
+        name: "clemetentine pillow",
+        images: [pillow_2_1, pillow_2_2],
+        brand: "Bentinit",
+        quantity: 3,
+        price: "80",
+        category: "pillow",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam expedita debitis illum totam esse, libero illo laborum. Doloremque ducimus quae vel labore unde, aliquid excepturi esse fugiat, nostrum fugit libero iusto? Dolorum, aliquid ipsum. Illum officiis delectus nulla repellendus corporis."
+    },
+    {
+        name: "otter wax boot wax",
+        images: [polish_1],
+        brand: "otter wax",
+        // quantity: 3,
+        price: "10",
+        category: "polish",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        instructions: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam expedita debitis illum totam esse, libero illo laborum. Doloremque ducimus quae vel labore unde, aliquid excepturi esse fugiat, nostrum fugit libero iusto? Dolorum, aliquid ipsum. Illum officiis delectus nulla repellendus corporis."
+    },
+    {
+        name: "otter wax saddle soap",
+        images: [polish_2],
+        brand: "otter wax",
+        // quantity: 3,
+        price: "10",
+        category: "polish",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        instructions: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam expedita debitis illum totam esse, libero illo laborum. Doloremque ducimus quae vel labore unde, aliquid excepturi esse fugiat, nostrum fugit libero iusto? Dolorum, aliquid ipsum. Illum officiis delectus nulla repellendus corporis."
+    },
+    {
+        name: "otter wax leather salve",
+        images: [polish_3],
+        brand: "otter wax",
+        // quantity: 3,
+        price: "10",
+        category: "polish",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        instructions: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam expedita debitis illum totam esse, libero illo laborum. Doloremque ducimus quae vel labore unde, aliquid excepturi esse fugiat, nostrum fugit libero iusto? Dolorum, aliquid ipsum. Illum officiis delectus nulla repellendus corporis."
+    },
+    {
+        name: "otter wax leather care kit",
+        images: [polish_4],
+        brand: "otter wax",
+        // quantity: 3,
+        price: "10",
+        category: "polish",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        instructions: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam expedita debitis illum totam esse, libero illo laborum. Doloremque ducimus quae vel labore unde, aliquid excepturi esse fugiat, nostrum fugit libero iusto? Dolorum, aliquid ipsum. Illum officiis delectus nulla repellendus corporis."
+    },
+    {
+        name: "ximena tote bag",
+        images: [tote_1_1, tote_1_2],
+        brand: "totee",
+        // quantity: 3,
+        price: "160",
+        category: "tote bag",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        details: {
+            width: "17\"",
+            height: "12\"",
+            bottom_gusset: "5.25\"",
+            exterior_pocket: "8\" x 7\"",
+            interior_pocket: "8.5\" x 5\"",
+        }
+    },
+    {
+        name: "chrisean tote bag",
+        images: [tote_2_1, tote_2_2],
+        brand: "totee",
+        // quantity: 3,
+        price: "170",
+        category: "tote bag",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        details: {
+            width: "17\"",
+            height: "12\"",
+            bottom_gusset: "5.25\"",
+            exterior_pocket: "8\" x 7\"",
+            interior_pocket: "8.5\" x 5\"",
+        }
+    },
+    {
+        name: "room mist",
+        images: [candle_1_1, candle_1_2],
+        brand: "atlanta candle co.",
+        // quantity: 3,
+        price: "25",
+        category: "cacndle and scent",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        about: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, nostrum! Quidem totam dolores voluptatum porro voluptate officiis nemo accusamus debitis nihil, voluptates, esse itaque repellendus, vitae doloremque labore obcaecati explicabo.",
+    },
+    {
+        name: "scenty candle",
+        images: [candle_2],
+        brand: "atlanta candle co.",
+        // quantity: 3,
+        price: "25",
+        category: "cacndle and scent",
+        reviewRate: getRandomNumber(),
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem!",
+        about: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, nostrum! Quidem totam dolores voluptatum porro voluptate officiis nemo accusamus debitis nihil, voluptates, esse itaque repellendus, vitae doloremque labore obcaecati explicabo.",
     },
 ]
